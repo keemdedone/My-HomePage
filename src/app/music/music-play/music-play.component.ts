14e614618d01dc554de6579af8a4215788e39c8d
keemdedone <komchan447@gmail.com>
2022-05-25 11:44:46 +0700

    display name and fix change music bug

--- src/app/music/music-play/music-play.component.ts
+++ src/app/music/music-play/music-play.component.ts
@@ -8,19 +8,32 @@ import { Component, OnInit  } from '@angular/core';
 export class MusicPlayComponent implements OnInit {
 
   audio = new Audio();
-  url: string[] = ["../assets/music/OnOurway.m4a","../assets/music/AerithTheme.m4a","../assets/music/Somnus.m4a"]
+
+  path: Array<{id: number,name: string, url: string}> = [
+    {id: 1,name: 'On ourway', url: "../assets/music/OnOurway.m4a"},
+    {id: 2,name: 'The Cetra', url: "../assets/music/AerithTheme.m4a"},
+    {id: 3,name: 'Somnus', url: "../assets/music/Somnus.m4a"},
+  ];
+
   play: boolean = true;
   select_num: number = 0;
 
   constructor() { }
 
   ngOnInit(): void {
+    // const arr: Array<{id: number, text: string}> = [
+    //   {id: 1, text: 'Sentence 1'},
+    //   {id: 2, text: 'Sentence 2'},
+    //   {id: 3, text: 'Sentence 3'},
+    //   {id: 4, text: 'Sentenc4 '},
+    // ];
+    // console.log(arr.length)
     return
   }
 
   onPlay(){
-    this.audio.src = this.url[this.select_num];
-    console.log(this.select_num,this.audio.src)
+    this.audio.src = this.path[this.select_num-1].url;
+    localStorage.setItem('music',this.select_num.toString());
     this.audio.load();
     this.audio.play();
     this.play = false;
@@ -32,14 +45,20 @@ export class MusicPlayComponent implements OnInit {
   }
 
   onNext(){
-    if(this.select_num = this.url.length -1){
-      this.select_num = this.url.length -1
+    this.audio.pause();
+    this.audio.currentTime = 0;
+    this.play = true;
+    if(this.select_num == this.path.length - 1){
+      this.select_num = this.path.length - 1
     } else {
       this.select_num = this.select_num + 1;
     }
   }
 
   onPrevious(){
+    this.audio.pause();
+    this.audio.currentTime = 0;
+    this.play = true;
     if(this.select_num > 0){
       this.select_num = this.select_num - 1;
     } else {
