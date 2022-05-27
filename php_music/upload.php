060b5c085834d447f41a79a8207cf06b29a86efb
keemdedone <komchan447@gmail.com>
2022-05-27 10:14:22 +0700

    fix redirect when upload

--- php_music/upload.php
+++ php_music/upload.php
@@ -4,9 +4,13 @@
         $music_loc = $_FILES['files']['tmp_name'];
         $music_folder = "C:/xampp/htdocs/my-homepage/src/assets/music/";
         if(move_uploaded_file($music_loc,$music_folder.$music)){
-            $music.json_encode("Complete");
+            $music.json_encode("complete");
+            header( "location: http://localhost:4200/music/music-play" );
+            exit(0);
         } else {
             $music.json_encode("fail");
+            header( "location: http://localhost:4200/music/music-play" );
+            exit(0);
         }
     }
 ?>
