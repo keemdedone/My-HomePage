0b401df26b57f8b955998d40a6e7124fd391d59b
keemdedone <komchan447@gmail.com>
2022-05-28 13:25:21 +0700

    add css to movie popular

--- src/app/movie/movie-popular/movie-popular.component.ts
+++ src/app/movie/movie-popular/movie-popular.component.ts
@@ -9,6 +9,8 @@ import { Popular } from 'src/app/model/movie';
 export class MoviePopularComponent implements OnInit {
   @Input() data: Popular | null =null ;
 
+  IMG_BASE_PATH = 'https://image.tmdb.org/t/p/original/';
+
   constructor() { }
 
   ngOnInit(): void {
